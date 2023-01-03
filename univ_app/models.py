637364257a390ce9cb17a471ae754542b3c58033
moipo <moipochtoviyyashik@inbox.ru>
2023-01-03 18:22:42 +0300

    saved state before new branch

--- univ_app/models.py
+++ univ_app/models.py
@@ -15,7 +15,7 @@ class StGroup(models.Model):
     title = models.CharField(max_length=200, default = '', blank = True)
     subject = models.ForeignKey("Subject", blank = True, null = True, on_delete = models.SET_NULL)
     student = models.ForeignKey("Student", blank = True, null = True, on_delete = models.SET_NULL)
-    
+
     def __str__(self):
         return self.title
 
